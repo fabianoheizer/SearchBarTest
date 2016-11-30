261d4a75993b51cccf5a5ee9ae46519f91c3ecd2
Fabiano Heizer <fabianoheizer@gmail.com>
2016-11-29 22:01:37 -0200

    Initial commit

---
+++ DTCCodes.swift
@@ -0,0 +1,21 @@
+//
+//  DTCCodes.swift
+//  SearchBarTest
+//
+//  Created by Fabiano Heizer on 03/10/16.
+//  Copyright © 2016 Fabiano Heizer. All rights reserved.
+//
+
+import Foundation
+
+
+class DTCCodes{
+    
+    var code :String
+    var description :String
+    
+    init(code: String, description: String) {
+        self.code = code
+        self.description = description
+    }
+}
---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchBarTest
+//
+//  Created by Fabiano Heizer on 02/10/16.
+//  Copyright © 2016 Fabiano Heizer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
